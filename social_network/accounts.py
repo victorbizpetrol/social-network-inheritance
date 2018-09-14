43b62a2596adf460e15b20f12f396fb82941fd2d
Victor Castillo <victor@bizpetrol.com>
2018-09-14 04:42:21 +0000

    All tests passed

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,30 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
+        post.set_user(self)
 
     def get_timeline(self):
         pass
+        timeline = []
+        for user in self.following:
+            timeline += user.posts
+        return timeline
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+
+    def __str__(self):
+        return "User: {} {}".format(self.first_name, self.last_name)
+
+    def __repr__(self):
+        return '<User: "{} {}">'.format(self.first_name, self.last_name)
+        
+#test script:  py.test test_accounts.py -k user_timeline
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,54 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp.strftime('%A, %b %d, %Y')
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
+
+    def __repr__(self):
+        return '<{}: {}>'.format(self.__class__.__name__, self.text)
+        
+        
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+        
+    def __str__(self):
+        return '@{} {}: \"{}\"\n\t{}'.format(
+                                        self.user.first_name, 
+                                        self.user.last_name, 
+                                        self.text, 
+                                        self.timestamp)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
+
+    def __str__(self):
+        return '@{} {}: \"{}\"\n\t{}\n\t{}'.format(
+                                            self.user.first_name, 
+                                            self.user.last_name, 
+                                            self.text, self.image_url, 
+                                            self.timestamp)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
 
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        return '@{} Checked In: \"{}\"\n\t{}, {}\n\t{}'.format(
+                                                            self.user.first_name, 
+                                                            self.text, 
+                                                            self.latitude, 
+                                                            self.longitude, 
+                                                            self.timestamp)
